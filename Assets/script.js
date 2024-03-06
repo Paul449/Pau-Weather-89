19a24186ce84dcd6eeef875c5ea639a4304ef287
paul449 <paul_bilbatua@hotmail.com>
2024-03-05 22:55:25 -0600

    new changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-let APIkey;
+let APIkey = 'd1c6aef6b187c5b413327fb226fc646f';
 //variables for storing data from Weather API;
 //constructing the weather query to make the API call
 //web API variables to select HTML elements and add interactivity
@@ -11,34 +11,56 @@ let cityWind = document.getElementById('Wind'); //targets the HTML element where
 let cityHumidity = document.getElementById('Humidity'); //targets the HTML element where is the humidity located on the current city info container (HTML Element)
 let fiveDayForecast = document.getElementsByClassName('Five-Day-Forecast-section'); //targets the html element where will be displayed the future conditions
 /*fetch request to openWeatherAPI */
+let currentDate = new Date();
 let present = function (city){ // this function will perform a request to the openWeatherAPI and get a response back
    let requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`; //API link to especifically select cities info using my API key
    fetch(requestURL).then(function(answer){ //sending the actual request to API to get information about cities before choosing how to get the response back
    return answer.json();
    }).then(data => {console.log(data) 
-      cityName.textContent = data.name; //
-      cityTemp.textContent = data.main.temp;
-      cityWind.textContent = data.wind.speed;
-      cityHumidity.textContent = data.main.humidity;
-      localStorage.setItem('enter', city);
-      var enter = localStorage.getItem('enter');
-   })   
+      cityName.textContent = data.name; // targeting city name from the json
+      cityTemp.textContent = data.main.temp; // targeting current temperature
+      cityWind.textContent = data.wind.speed; // targeting current wind speed
+      cityHumidity.textContent = data.main.humidity; // targeting current humidity
+      localStorage.setItem('enter', city); // saving city name on the local storage
+      var enter = localStorage.getItem('enter'); // get item from local storage
+      let lon;
+      let lat;
+      //fetch().then()
+   })
 };
-present('San Francisco');
-searchBtn.addEventListener('click', present);
+searchBtn.addEventListener('click', function(e){
+   e.preventDefault();
+   var inputCity = InputText.value.trim();
+   present(inputCity);
+}); 
 
-/*let future = function(lon,lat){
-   var futureURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIkey}`;
-   fetch(futureURL).then(answer =>{answer.json()})
-   .then(data => {console.log(data)})
+let future = function(lon,lat){ // five forecast weather
+   var futureURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`;
+   fetch(futureURL).then(answer =>{
+      return answer.json();
+})
+   .then(data => {console.log(data)
+   
+      let T2 = document.getElementById('T2'); //
+      let W2 = document.getElementById('W2');
+      let H2 = document.getElementById('H2');
+      let T3 = document.getElementById('T3');
+      let W3 = document.getElementById('W3');
+      let H3 = document.getElementById('H3');
+      let T4 = document.getElementById('T4');
+      let W4 = document.getElementById('W4');
+      let H4 = document.getElementById('H4');
+      let T5 = document.getElementById('T5');
+      let W5 = document.getElementById('W5');
+      let H5 = document.getElementById('H5'); 
+   })
 }
-var callFuture = future(-97.7431, 30.2672);*/
-
+future()
 let storeCity = function(){
   let value = InputText.value;
-  searchHistory.textContent = InputText.value;
+  searchHistory.textContent = value;
   localStorage.setItem('history', value);
+  let history = localStorage.getItem('history');
 }
-let history = localStorage.getItem('history');
-searchBtn.addEventListener('click', storeCity, true);
+searchBtn.addEventListener('click', storeCity);
 
--- README.md
+++ README.md
@@ -3,15 +3,30 @@
 This web app is about displaying different weather conditions form any specific city from any country obtaining information from a server-side api containing information related to weather
 
 # Description
+The purpose of this web application is to show weather data to anyone travelling to a specific city or town;
+including the option to see the forecast for the next 5 days.
 
 # Installation
+N/A
 
 # Usage
 
+This is only accessed by using any web browser and anyone can have access to it and see current and future weather.
+This is not a mobile app or desktop app where we could be able to download it but we can still use it if we have internet connection.
+
 # Screenshot
+
+![Screenshot](/Assets/weather%20dashborard.png)
 
 # Links
 
+(https://github.com/Paul449/Pau-Weather-89)
+()
+
 # Credits
 
+N/A
+
 # License
+
+(https://github.com/Paul449/Pau-Weather-89/blob/main/LICENSE)
