4e2d8dea5b370d39841e39f27688fc593f39d67d
paul449 <paul_bilbatua@hotmail.com>
2024-05-31 14:49:26 -0500

    new changes

--- Assets/script.js
+++ Assets/script.js
@@ -1,66 +1,84 @@
 let APIkey = 'd1c6aef6b187c5b413327fb226fc646f';
-//variables for storing data from Weather API;
-//constructing the weather query to make the API call
-//web API variables to select HTML elements and add interactivity
-let InputText = document.getElementById('any-location'); // targeting the input box where we enter our city name
-let searchBtn = document.getElementById('submitBtn'); // targeting the button for searching cities
-let searchHistory = document.getElementById('first-city'); //targets the search history after selecting a city (HTML Element)
-let cityName = document.getElementById('City-Name'); //targets the HTML element where is the city name located on the current city info container (HTML Element)
-let cityTemp = document.getElementById('temp'); //targets the HTML element where is the temperature located on the current city info container (HTML Element)
-let cityWind = document.getElementById('Wind'); //targets the HTML element where is the wind speed located on the current city info container (HTML Element)
-let cityHumidity = document.getElementById('Humidity'); //targets the HTML element where is the humidity located on the current city info container (HTML Element)
-let fiveDayForecast = document.getElementsByClassName('Five-Day-Forecast-section'); //targets the html element where will be displayed the future conditions
-/*fetch request to openWeatherAPI */
-let currentDate = new Date();
-let present = function (city){ // this function will perform a request to the openWeatherAPI and get a response back
-   let requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`; //API link to especifically select cities info using my API key
-   fetch(requestURL).then(function(answer){ //sending the actual request to API to get information about cities before choosing how to get the response back
-   return answer.json();
-   }).then(data => {console.log(data) 
-      cityName.textContent = data.name; // targeting city name from the json
-      cityTemp.textContent = data.main.temp; // targeting current temperature
-      cityWind.textContent = data.wind.speed; // targeting current wind speed
-      cityHumidity.textContent = data.main.humidity; // targeting current humidity
-      localStorage.setItem('enter', city); // saving city name on the local storage
-      var enter = localStorage.getItem('enter'); // get item from local storage
-      let lon;
-      let lat;
-      //fetch().then()
-   })
-};
-searchBtn.addEventListener('click', function(e){
-   e.preventDefault();
-   var inputCity = InputText.value.trim();
-   present(inputCity);
-}); 
-
-let future = function(lon,lat){ // five forecast weather
-   var futureURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIkey}`;
-   fetch(futureURL).then(answer =>{
-      return answer.json();
+let city; // variable to store any city
+let searchbox = document.querySelector('.future-conditions'); // display five days forecast
+let InputBox = document.getElementById('input-txt'); // enter city
+let fetchBtn = document.getElementById('searchBtn'); // click button
+function getCity () {
+  //present conditions
+    searchbox.innerHTML = ""; // it updates data when looking for multiple cities instead of appending the conditions
+    const requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIkey}`; // requesting city on the openWeather api by using my API key
+    fetch(requestURL) // requesting data from api for any city (present values)
+    .then(response => response.json()) // specifying the response in JSON
+    .then(data =>{ // data displayed json format
+          document.querySelector('#City').innerHTML = data.name; // targeting the city name
+          document.querySelector('#Temp').innerHTML = data.main.temp; // targeting the current temperature
+          document.querySelector('#Wind').innerHTML = data.wind.speed; // targeting the current wind speed
+          document.querySelector('#Humid').innerHTML = data.main.humidity; // targeting the current humidity
+    }).catch(err =>{
+      console.error('error:',err)
+    })
+// future conditions
+const futureURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIkey}`;
+    fetch(futureURL) // requesting data
+    .then(response=> response.json()) // specifying how I am going to receive the response
+    .then(data => { // printing data
+      const fiveDays = []; // storing future forecast
+      for(let i = 0; i < data.list.length; i++){
+        const date = new Date(data.list[i].dt_txt);
+        const day = date.getDate();
+        if(!fiveDays.includes(day)){
+          let box = document.createElement('div'); // future forecast container
+          box.setAttribute('style','border: 2px solid blue; background-color:#1E90FF; color:white; margin-right:10px; padding:20px');
+          box.classList.add('flex-container');
+          let FD = document.createElement('h1');
+          let FT = document.createElement('p');
+          let FW = document.createElement('p');
+          let FH = document.createElement('p');
+          box.appendChild(FD);
+          box.appendChild(FT);
+          box.appendChild(FW);
+          box.appendChild(FH);
+          searchbox.appendChild(box);
+          FD.textContent = data.list[i].dt_txt;
+          FT.textContent = 'Temp: ' + data.list[i].main.temp +' °F' ;
+          FW.textContent="Wind: " + data.list[i].wind.speed + ' mph';
+          FH.textContent="Humidity: " + data.list[i].main.humidity + ' %';
+          fiveDays.push(day);
+        }
+        if(fiveDays.length >=5){
+          break; // stop after passing 5 days
+        }
+      }
+}).catch(err => {
+  console.error('error',err);
 })
-   .then(data => {console.log(data)
-   
-      let T2 = document.getElementById('T2'); //
-      let W2 = document.getElementById('W2');
-      let H2 = document.getElementById('H2');
-      let T3 = document.getElementById('T3');
-      let W3 = document.getElementById('W3');
-      let H3 = document.getElementById('H3');
-      let T4 = document.getElementById('T4');
-      let W4 = document.getElementById('W4');
-      let H4 = document.getElementById('H4');
-      let T5 = document.getElementById('T5');
-      let W5 = document.getElementById('W5');
-      let H5 = document.getElementById('H5'); 
-   })
-}
-future()
-let storeCity = function(){
-  let value = InputText.value;
-  searchHistory.textContent = value;
-  localStorage.setItem('history', value);
-  let history = localStorage.getItem('history');
-}
-searchBtn.addEventListener('click', storeCity);
-
+};
+fetchBtn.addEventListener('click', (event) =>{
+    let list = document.querySelector('.city-list');
+    let Paragraph = document.createElement('button');
+    Paragraph.setAttribute('style','border:3px solid grey;border-radius:5px; position: relative; left: 10px; background-color: grey; padding-right: 90px;margin-bottom:10px; color:white;')
+    list.append(Paragraph);
+    event.preventDefault(); // prevent losing data after refreshing website
+    city = InputBox.value.trim();
+    localStorage.setItem('enter',city);
+    console.log(localStorage.getItem('enter'));
+    Paragraph.innerHTML = city;
+    Paragraph.addEventListener('click', getCity);
+    getCity(city);
+     });
+    /* let box = document.createElement('div'); // future forecast container
+    box.setAttribute('style','border: 2px solid blue; background-color:#1E90FF; color:white; margin-right:10px; padding:20px');
+    box.classList.add('flex-container');
+    let FD = document.createElement('h1');
+    let FT = document.createElement('p');
+    let FW = document.createElement('p');
+    let FH = document.createElement('p');
+    box.appendChild(FD);
+    box.appendChild(FT);
+    box.appendChild(FW);
+    box.appendChild(FH);
+    searchbox.appendChild(box);
+    FD.textContent = element.dt_txt;
+    FT.textContent = 'Temp: ' + element.main.temp +' °F' ;
+    FW.textContent="Wind: " + element.wind.speed + ' mph';
+    FH.textContent="Humidity: " + element.main.humidity + ' %';*/
